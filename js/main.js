8673979be1c4e879a82d51075604672f048a1688
unknown <singhhardy724@gmail.comcleargit config --global user.name Hardik>
2021-02-11 10:22:51 -0800

    Initial

--- js/main.js
+++ js/main.js
@@ -21,6 +21,24 @@ document.addEventListener("DOMContentLoaded", function(event) {
   });
 
 
-  // tESTIMONIALS
+// Contact
+
+jQuery(function ($) {
+  function init_map1() {
+      var myLocation = new google.maps.LatLng(38.885516, -77.09327200000001);
+      var mapOptions = {
+          center: myLocation,
+          zoom: 16
+      };
+      var marker = new google.maps.Marker({
+          position: myLocation,
+          title: "Property Location"
+      });
+      var map = new google.maps.Map(document.getElementById("map1"),
+          mapOptions);
+      marker.setMap(map);
+  }
+  init_map1();
+});
 
   
